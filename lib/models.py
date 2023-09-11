dbf109cec90f5ad8245961c1e046ee5abc5f1812
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-09-11 17:10:11 +0300

    Added Customer model defined relationship with SaleRepresentative

--- lib/models.py
+++ lib/models.py
@@ -2,8 +2,9 @@
 
 from datetime import datetime
 from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy import (Column, Integer, String, 
-    Float, DateTime, CheckConstraint, UniqueConstraint)
+    Float, ForeignKey, DateTime, CheckConstraint, UniqueConstraint)
 
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -19,6 +20,9 @@ class SaleRepresentative(Base):
     email = Column(String(50), nullable=False, unique=True)
     region = Column(String())
     phone_number = Column(Integer(), unique=True, nullable=False)
+
+    #Establish a one-to-many relationship between SaleRepresentative and Customer
+    customers = relationship("Customer", back_populates='salesrep')
 
     def __repr__(self):
         return f"Sale_Representative {self.id}:"\
@@ -40,3 +44,14 @@ class Product(Base):
             + f"Name: {self.name}, " \
             + f"Price: {self.price}"
     
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(), nullable=False)
+    phone_number = Column(Integer(), nullable=False)
+    address = Column(String(), nullable=False)
+    sale_rep = Column(Integer(), ForeignKey('salesreps.id'))
+    
+    #Establish a many-to-one relationship between Customer and SaleRepresentative
+    salesrep = relationship('SaleRepresentative', back_populates='customers')
